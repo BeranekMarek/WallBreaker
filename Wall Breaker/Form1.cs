2641e375ebc67d7ce7359775bbbc144095ce7db6
ancablahova <ancablahova@post.cz>
2024-04-24 10:02:27 +0200

    Přidejte soubory projektu.

---
+++ Wall Breaker/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wall_Breaker
+{
+    public partial class Form1 : Form
+    {
+        // bitmapa do ktere kreslim
+        Bitmap mobjMyBitmap;
+        Graphics mobjPlatnoBackround;
+
+        // grafika na okne z pictureboxu
+        Graphics mobjPlatnoForm;
+
+        //kulicka
+        clsKulicka mobjKulicka;
+
+        // random button
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //----------------------------------------------------------------------------
+        // nahrani formu do pameti
+        //----------------------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // vytvoreni grafiky z pisctureboxu
+            mobjPlatnoForm = pbPlatno.CreateGraphics();
+
+            // vytvorenji bitmapy
+            mobjMyBitmap = new Bitmap(pbPlatno.Width,pbPlatno.Height);
+            mobjPlatnoBackround = Graphics.FromImage(mobjMyBitmap);
+
+            // vytvorit kulicku
+            mobjKulicka = new clsKulicka(50, 50, 2, 10);
+
+            // nastaveni timeru prekresleni
+            tmrRedraw.Interval = 10;
+            tmrRedraw.Enabled = true;
+        }
+
+
+        //----------------------------------------------------------------------------
+        // prekresleni obrazu
+        //----------------------------------------------------------------------------
+        private void tmrRedraw_Tick(object sender, EventArgs e)
+        {
+            //vymazat platno - pohyb
+            mobjPlatnoBackround.Clear(Color.White);
+
+            // nakresli kolecko
+            mobjKulicka.VykresliSe();
+
+            // posun kulicky
+            mobjKulicka.PosunSe();
+            
+            // vykesleni na pbPlatno
+            mobjPlatnoForm.DrawImage(mobjMyBitmap, 0, 0);
+
+        }
+    }
+}
---
+++ Wall Breaker/clsKulicka.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wall_Breaker
+{
+    internal class clsKulicka
+    {
+        // kreslici platno
+        Graphics mobjPlatno;
+
+        // souradnice kulicky
+        int mintKulickaX, mintKulickaY;
+        int mintKulickaPosunX, mintKulickaPosunY;
+        int mintKulickaPolomer = 10;
+
+        public clsKulicka(int intKulickaX, int intKulickaY,
+            int intKulickaPosun, int intKulickaPolomer,
+            Graphics objPlatno)
+        {
+            mintKulickaX = intKulickaX;
+            mintKulickaY = intKulickaY;
+            mintKulickaPosunX = intKulickaPosun;
+            mintKulickaPosunY = intKulickaPosun;
+            mintKulickaPolomer = intKulickaPolomer;
+
+            mobjPlatno = objPlatno;
+        }
+
+        //----------------------------------------------------------------------------
+        // vykresleni
+        //----------------------------------------------------------------------------
+        public void VykresliSe()
+        {
+            mobjPlatno.FillEllipse(Brushes.Blue, mintKulickaX, mintKulickaY, 
+                mintKulickaPolomer, mintKulickaPolomer);
+
+        }
+
+        //----------------------------------------------------------------------------
+        // posun a kolize
+        //----------------------------------------------------------------------------
+        public void PosunSe()
+        {
+            mintKulickaX = mintKulickaX + mintKulickaPosunX;
+            mintKulickaY = mintKulickaY + mintKulickaPosunY;
+
+            // kolize
+            if ((mintKulickaY + mintKulickaPolomer) > mobjPlatno.VisibleClipBounds.Height)
+            {
+                mintKulickaPosunY = mintKulickaPosunY * (-1);
+            }
+
+            if ((mintKulickaX + mintKulickaPolomer) > mobjPlatno.VisibleClipBounds.Width)
+            {
+                mintKulickaPosunX = mintKulickaPosunX * (-1);
+            }
+
+            if (mintKulickaY < 0)
+            {
+                mintKulickaPosunY = mintKulickaPosunY * (-1);
+            }
+
+            if (mintKulickaX < 0)
+            {
+                mintKulickaPosunX = mintKulickaPosunX * (-1);
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+}
